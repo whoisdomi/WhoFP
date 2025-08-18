64d72d187919a813a77c00160d5ca7e5dee3d220
whoisdomi <dcantu83@gmail.com>
2025-08-18 09:21:00 -0500

    Ioniq 6 Values
    Ioniq 6 Values
    
    Ioniq 6 Values
    
    Update test_hyundai_canfd.py 720
    
    Update safety_hyundai_canfd.h 720
    
    Ioniq 6 Values
    
    Ioniq 6 Values
    
    Taco Steer 3/10
    Max RT 90
    Driver Allowance 250
    
    kP stock to 1.0
    
    Sets stock kP to 1.0. Added this because firestar sets his stock to .6
    
    steer up 3 at highway
    
    Steer up 3 to 5
    
    384 torque highway
    
    Update safety_hyundai_canfd.h
    
    Ioniq 6 Values
    
    Ioniq 6 Values
    
    Update test_hyundai_canfd.py 720
    
    Update safety_hyundai_canfd.h 720
    
    Ioniq 6 Values
    
    Ioniq 6 Values
    
    Taco Steer 3/10
    Max RT 90
    Driver Allowance 250
    
    kP stock to 1.0
    
    Sets stock kP to 1.0. Added this because firestar sets his stock to .6
    
    steer up 3 at highway
    
    Steer up 3 to 5
    
    384 torque highway
    
    Update safety_hyundai_canfd.h

--- selfdrive/car/hyundai/values.py
+++ selfdrive/car/hyundai/values.py
@@ -26,19 +26,19 @@ class CarControllerParams:
     self.STEER_STEP = 1  # 100 Hz
 
     if CP.carFingerprint in CANFD_CAR and frogpilot_toggles and frogpilot_toggles.taco_tune_hacks:
-      self.STEER_MAX = 384 if vEgoRaw < 11. else 330
-      self.STEER_DRIVER_ALLOWANCE = 350
-      self.STEER_DRIVER_MULTIPLIER = 2
-      self.STEER_THRESHOLD = 350
-      self.STEER_DELTA_UP = 10 if vEgoRaw < 11. else 2
-      self.STEER_DELTA_DOWN = 10 if vEgoRaw < 11. else 3
-    elif CP.carFingerprint in CANFD_CAR:
-      self.STEER_MAX = 270
+      self.STEER_MAX = 720 if vEgoRaw < 14. else 400
       self.STEER_DRIVER_ALLOWANCE = 250
       self.STEER_DRIVER_MULTIPLIER = 2
       self.STEER_THRESHOLD = 250
-      self.STEER_DELTA_UP = 2
-      self.STEER_DELTA_DOWN = 3
+      self.STEER_DELTA_UP = 5 if vEgoRaw < 11. else 3
+      self.STEER_DELTA_DOWN = 10 if vEgoRaw < 11. else 3
+    elif CP.carFingerprint in CANFD_CAR:
+      self.STEER_MAX = 384
+      self.STEER_DRIVER_ALLOWANCE = 250
+      self.STEER_DRIVER_MULTIPLIER = 2
+      self.STEER_THRESHOLD = 250
+      self.STEER_DELTA_UP = 4
+      self.STEER_DELTA_DOWN = 10
 
     # To determine the limit for your car, find the maximum value that the stock LKAS will request.
     # If the max stock LKAS request is <384, add your car to this list.
