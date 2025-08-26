7bff2f263f6b7fd19c5a5f59406a952ee6dac1d3
whoisdomi <dcantu83@gmail.com>
2025-08-26 12:20:59 -0500

    steer up 3 at highway

--- selfdrive/car/hyundai/values.py
+++ selfdrive/car/hyundai/values.py
@@ -30,7 +30,7 @@ class CarControllerParams:
       self.STEER_DRIVER_ALLOWANCE = 250
       self.STEER_DRIVER_MULTIPLIER = 2
       self.STEER_THRESHOLD = 250
-      self.STEER_DELTA_UP = 5 if vEgoRaw < 11. else 2
+      self.STEER_DELTA_UP = 5 if vEgoRaw < 11. else 3
       self.STEER_DELTA_DOWN = 10 if vEgoRaw < 11. else 3
     elif CP.carFingerprint in CANFD_CAR:
       self.STEER_MAX = 610
