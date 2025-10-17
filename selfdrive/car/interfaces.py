b36a5fc6430f5431e42f2fc5992442e5d8812b60
firestar5683 <168790843+firestar5683@users.noreply.github.com>
2025-10-17 15:26:51 -0500

    Modify torque tuning parameters in interfaces.py
    Adjusted torque tuning parameters for improved performance.

--- selfdrive/car/interfaces.py
+++ selfdrive/car/interfaces.py
@@ -302,10 +302,10 @@ class CarInterfaceBase(ABC):
 
     tune.init('torque')
     tune.torque.useSteeringAngle = use_steering_angle
-    tune.torque.kf = 1.0
+    tune.torque.kf = 0.65
     tune.torque.kp = 0.6
-    tune.torque.ki = 0.3
-    tune.torque.kd = 0.0
+    tune.torque.ki = 1.0
+    tune.torque.kd = 0.75
     tune.torque.friction = params['FRICTION']
     tune.torque.latAccelFactor = params['LAT_ACCEL_FACTOR']
     tune.torque.latAccelOffset = 0.0
