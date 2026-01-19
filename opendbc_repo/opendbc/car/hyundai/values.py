76566d8fbd6bb4cdbd5f32203064d4f553051dc0
whoisdomi <dcantu83@gmail.com>
2026-01-19 12:44:41 -0600

    Values 400/3/3
    Adjusted steering parameters for low and high speed scenarios in taco tune hack.

--- opendbc_repo/opendbc/car/hyundai/values.py
+++ opendbc_repo/opendbc/car/hyundai/values.py
@@ -27,12 +27,12 @@ class CarControllerParams:
     # Taco tune hack - speed dependent limits (FrogPilot)
     if CP.flags & HyundaiFlags.CANFD and taco_tune_hack:
       if vEgoRaw < 15.:  # Low speed (~34 mph) - maximum aggression
-        self.STEER_MAX = 600
+        self.STEER_MAX = 400
         self.STEER_DELTA_UP = 3
-        self.STEER_DELTA_DOWN = 5
+        self.STEER_DELTA_DOWN = 3
       else:  # High speed - slightly conservative
-        self.STEER_MAX = 384
-        self.STEER_DELTA_UP = 3
+        self.STEER_MAX = 400
+        self.STEER_DELTA_UP = 2
         self.STEER_DELTA_DOWN = 3
       self.STEER_DRIVER_ALLOWANCE = 350
       self.STEER_DRIVER_MULTIPLIER = 2
