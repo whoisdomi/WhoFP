a3d9eb088e3b7fcfe14eb594028637ce5d0ec667
whoisdomi <dcantu83@gmail.com>
2026-02-07 15:28:54 -0600

    util

--- common/util.cc
+++ common/util.cc
@@ -304,11 +304,7 @@ std::string check_output(const std::string& command) {
 bool system_time_valid() {
   // Default to August 26, 2024
  //tm min_tm = {.tm_year = 2024 - 1900, .tm_mon = 7, .tm_mday = 26};
-  tm min_tm = {0};
-  min_tm.tm_mday = 26;
-  min_tm.tm_mon = 7;
-  min_tm.tm_year = 2024 - 1900;
-  time_t min_date = mktime(&min_tm);
+  tm min_tm = {.tm_year = 2024 - 1900, .tm_mon = 6, .tm_mday = 26};
 
   struct stat st;
   if (stat("/lib/systemd/systemd", &st) == 0) {
