f5f983914d072c275e10b2fd300377666d90b521
whoisdomi <dcantu83@gmail.com>
2026-02-15 21:07:39 -0600

    match values

--- opendbc_repo/opendbc/safety/tests/test_hyundai_canfd.py
+++ opendbc_repo/opendbc/safety/tests/test_hyundai_canfd.py
@@ -34,8 +34,8 @@ class TestHyundaiCanfdBase(HyundaiButtonBase, common.CarSafetyTest, common.Drive
 
   MAX_RT_DELTA = 150
 
-  DRIVER_TORQUE_ALLOWANCE = 350
-  DRIVER_TORQUE_FACTOR = 3
+  DRIVER_TORQUE_ALLOWANCE = 250
+  DRIVER_TORQUE_FACTOR = 2
 
   # Safety around steering req bit
   MIN_VALID_STEERING_FRAMES = 89
